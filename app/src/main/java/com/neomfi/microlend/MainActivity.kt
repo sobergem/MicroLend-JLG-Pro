9e38effd3cdc20a6d4c96e75a90ef3838138809b
sobergem <hariom.develop@gmail.com>
2026-03-25 20:13:14 +0530

    feat: implement Jetpack Compose navigation and Add Lead placeholder

--- app/src/main/java/com/neomfi/microlend/MainActivity.kt
+++ app/src/main/java/com/neomfi/microlend/MainActivity.kt
@@ -4,44 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.neomfi.microlend.presentation.dashboard.DashboardScreen
 import com.neomfi.microlend.ui.theme.MicroLendJLGProTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MicroLendJLGProTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+          MicroLendJLGProTheme{
+              DashboardScreen()
+          }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MicroLendJLGProTheme {
-        Greeting("Android")
-    }
-}
